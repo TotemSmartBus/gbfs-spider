b7a246060e85421d179e1b10b5d48b8f14e44965
xiaohaoxing <xiaohaoxing@outlook.com>
2023-01-29 22:05:18 +0800

    feat: 添加 GTFS 的数据解压

--- gtfs-spider/main.py
+++ gtfs-spider/main.py
@@ -1,7 +1,7 @@
 import json
 import requests
 import os
-
+import zipfile
 base_dir = os.getcwd()
 
 # 遇到已有的文件夹是否覆盖？
@@ -18,11 +18,18 @@ def save_data(link, code, target_dir, progress):
         fails.append(code)
         return
     print(progress, code, ' saving.')
-    with open(os.path.join(target_dir, code), 'wb') as f:
+    zip_filename = os.path.join(target_dir, code + '.zip')
+    with open(zip_filename, 'wb') as f:
         for chunk in resp.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
-    print(progress, code, ' saved.')
+    print(progress, code, ' unzipping.')
+    zip_file = zipfile.ZipFile(zip_filename)
+    zip_list = zip_file.namelist()  # 得到压缩包里所有文件
+    for f in zip_list:
+        zip_file.extract(f, target_dir)  # 循环解压文件到指定目录
+    zip_file.close()  # 关闭文件，必须有，释放内存
+    print(progress, code, ' done.')
 
 
 if __name__ == '__main__':
@@ -43,8 +50,8 @@ if __name__ == '__main__':
         progress = '[' + str(current) + '/' + str(size) + ']'
         if not filename.endswith('.json'):
             break
-        code = filename.split('\\.')[0]
-        target_dir = join(dataset_dir, filename)
+        code = filename.split('.')[0]
+        target_dir = join(dataset_dir, code)
         if not os.path.exists(target_dir):
             os.mkdir(target_dir)
         elif OVERWRITE:
